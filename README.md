e6be14c1e40921c63ca0a8459ff49168377c7fee
c0de-slayer <fsali315@gmail.com>
2023-02-01 18:44:40 +0530

    all

--- README.md
+++
@@ -1,1 +0,0 @@
-https://demo.kaliumtheme.com/photography/ url for template
--- static/js/main.js
+++ static/js/main.js
@@ -37,3 +37,16 @@ $(window).on('load', function() {
 	});
 })(jQuery);
 
+// code for backtotop button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+function topFunction() {
+	window.scrollTo({top: 0, behavior: 'smooth'});
+}
